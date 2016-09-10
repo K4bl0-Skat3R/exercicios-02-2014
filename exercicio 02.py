89bc6139f9a456188c511f11a6fd201dd2ec51a8
G3n1ls0nM3ss <gnewstudio@gmail.com>
2016-09-10 06:38:12 -0300

    Update exercicio 02.py

--- exercicio 02.py
+++ exercicio 02.py
@@ -6,7 +6,7 @@
 #exercicio 02
 numero = input ("digite um numero de 0 a 60: ")
 if numero == 60:
-	print "sucessor de 60 e zero"
+	print "sucessor de 60 eh zero"
 elif numero <=59: 
-	print "sucessor do numero digitado e", numero+1
+	print "sucessor do numero digitado eh: ", numero+1
 	 
